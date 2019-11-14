963421f694013f475b8a3058ed9586dd0b0b4a2e
jul <julislazy@gmail.com>
2019-11-14 12:47:56 +0100

    Fixed language level #9

--- bl2_skingen/imaging/build_multiply_sqrt.py
+++ bl2_skingen/imaging/build_multiply_sqrt.py
@@ -3,4 +3,4 @@ from distutils.core import Extension
 from Cython.Build import cythonize
 import numpy
 
-setup(name="multiply_sqrt", include_dirs = [numpy.get_include()], ext_modules = cythonize("multiply_sqrt.pyx") )
+setup(name="multiply_sqrt", include_dirs = [numpy.get_include()], ext_modules = cythonize("multiply_sqrt.pyx", language_level = "3") )
