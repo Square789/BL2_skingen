e75a9a3c41ed481949731f9c6bf3e55e622e51c9
Square789 <46634729+Square789@users.noreply.github.com>
2020-02-01 16:20:16 +0100

    Setup headache changes

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ from Cython.Build import cythonize
 import numpy # Just for get_include()
 
 NEEDED_MODULES = (
-	Extension("apply_decal", ["bl2_skingen/imaging/apply_decal.pyx"]),
-	Extension("blend_inplace", ["bl2_skingen/imaging/blend_inplace.pyx"]),
-	Extension("multiply_sqrt", ["bl2_skingen/imaging/multiply_sqrt.pyx"]),
-	Extension("ue_color_diff", ["bl2_skingen/imaging/ue_color_diff.pyx"]),
+	Extension("bl2_skingen.imaging.apply_decal", ["bl2_skingen/imaging/apply_decal.pyx"]),
+	Extension("bl2_skingen.imaging.blend_inplace", ["bl2_skingen/imaging/blend_inplace.pyx"]),
+	Extension("bl2_skingen.imaging.multiply_sqrt", ["bl2_skingen/imaging/multiply_sqrt.pyx"]),
+	Extension("bl2_skingen.imaging.ue_color_diff", ["bl2_skingen/imaging/ue_color_diff.pyx"]),
 )
 
 UNUSED_MODULES = (
-	Extension("darken", ["bl2_skingen/imaging/darken.pyx"]),
-	Extension("overlay", ["bl2_skingen/imaging/overlay.pyx"])
+	Extension("bl2_skingen.imaging.darken", ["bl2_skingen/imaging/darken.pyx"]),
+	Extension("bl2_skingen.imaging.overlay", ["bl2_skingen/imaging/overlay.pyx"])
 )
 
 to_compile = list(NEEDED_MODULES)
@@ -30,7 +30,7 @@ with open("README.md", "r") as h:
 
 setup(
 	name = "BL2_skingen",
-	version = "1.2.0",
+	version = "1.2.1",
 	description = "Utility to generate png files from Borderlands 2 in-game skin files.",
 	long_description = l_desc,
 	long_description_content_type = "text/markdown",
@@ -39,7 +39,7 @@ setup(
 	include_dirs = [numpy.get_include()],
 	entry_points = {
 		"console_scripts": [
-			"BL2_skingen = skingen:main",
+			"skingen = skingen:main",
 		]
 	},
 	url = "https://www.github.com/Square789/BL2_skingen/",
